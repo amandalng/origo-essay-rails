7cd3aebc8cb9ed6c49dbf125f4e332b60ec2a2f1
Amanda Ng <amandaleahng@gmail.com>
2020-05-11 23:01:49 +0800

    navbar fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_essays_path(current_user)
   end
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :full_name, :administrator])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :full_name, :administrator])
+    end
 end
