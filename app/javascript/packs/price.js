142bcb92b8576224bd469f94807b315cc547bcb8
Amanda Ng <amandaleahng@gmail.com>
2020-08-10 17:50:45 +0800

    price change

--- app/javascript/packs/price.js
+++ app/javascript/packs/price.js
@@ -3,6 +3,36 @@ const grad = document.getElementById('essay_applicant_type_graduate')
 const wordCount = document.getElementById('essay_word_count')
 const dynamicPrice = document.querySelector('.price')
 
+let price
+if (undergrad.checked == false && grad.checked == false) {
+  price = 0
+} else if (undergrad.checked === true) {
+  if (wordCount.value == "") {
+    price = 0
+  } else if (parseInt(wordCount.value) <= 250) {
+    price = 80
+  } else if (parseInt(wordCount.value) <= 400) {
+    price = 110
+  } else if (parseInt(wordCount.value) <= 600) {
+    price = 130
+  } else if (parseInt(wordCount.value) > 600) {
+    price = 180
+  }
+} else if (grad.checked === true) {
+  if (wordCount.value == "") {
+    price = 0
+  } else if (parseInt(wordCount.value) <= 250) {
+    price = 90
+  } else if (parseInt(wordCount.value) <= 400) {
+    price = 120
+  } else if (parseInt(wordCount.value) <= 600) {
+    price = 150
+  } else if (parseInt(wordCount.value) > 600) {
+    price = 200
+  }
+} else price = 0
+dynamicPrice.innerText = price
+
 wordCount.addEventListener("keyup", (e) => {
   let price
   if (undergrad.checked == false && grad.checked == false) {
@@ -56,13 +86,13 @@ grad.addEventListener("change", (e) => {
     if (wordCount.value == "") {
       price = 0
     } else if (parseInt(wordCount.value) <= 250) {
-      price = 80
+      price = 90
     } else if (parseInt(wordCount.value) <= 400) {
-      price = 110
+      price = 120
     } else if (parseInt(wordCount.value) <= 600) {
-      price = 130
+      price = 150
     } else if (parseInt(wordCount.value) > 600) {
-      price = 180
+      price = 200
     }
   dynamicPrice.innerText = price
 })
